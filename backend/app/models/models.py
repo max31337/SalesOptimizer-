abc36a297be11143b02ada089f51ae785b9849bf
max31337 <navarro.markanthony.tud@gmail.com>
2025-03-22 14:07:06 +0800

    added customers table 🚀🚀🚀

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -8,3 +8,12 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, nullable=True)
