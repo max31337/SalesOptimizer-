0ec9f96e6eb82d8230bbb16cf16ce18c8f14b259
max31337 <navarro.markanthony.tud@gmail.com>
2025-04-11 14:29:03 +0800

    v

--- salesoptimizer/public/assets/js/config.js
+++ salesoptimizer/public/assets/js/config.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
     apiUrl: process.env.NODE_ENV === 'production' 
-        ? "https://crossover.proxy.rlwy.net:32542/api"
+        ? "http://crossover.proxy.rlwy.net:32542/api"
         : "http://localhost:8000/api"
 };
