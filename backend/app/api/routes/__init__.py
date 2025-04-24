a5a2ac4bb6db80de55e11036e8b024f8c6420150
Mark Anthony Navarro <118058786+max31337@users.noreply.github.com>
2025-04-24 20:38:27 +0800

    Update __init__.py

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -6,10 +6,10 @@ from .crm import router as crm_router
 router = APIRouter()
 
 # Include all route modules
-router.include_router(auth_routes.router, prefix="/auth", tags=["authentication"])
-router.include_router(user_check_routes.router, prefix="/auth", tags=["authentication"])
-router.include_router(invited_user_routes.router, prefix="/auth", tags=["authentication"])
-router.include_router(password_reset_routes.router, prefix="/auth", tags=["authentication"])
+router.include_router(auth_routes.router, prefix="/auth", tags=["Authentication"])
+router.include_router(user_check_routes.router, prefix="/auth", tags=["Authentication"])
+router.include_router(invited_user_routes.router, prefix="/auth", tags=["Authentication"])
+router.include_router(password_reset_routes.router, prefix="/auth", tags=["Authentication"])
 
 # Admin routes
 router.include_router(user_management_routes.router, prefix="/admin", tags=["admin"])
